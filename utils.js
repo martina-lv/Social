f7e09ae3d54668661e12d97e7402bbd14753145b
martina-lv <livigni.martina@gmail.com>
2022-07-03 23:18:32 +0200

    fix: delete function on btn

--- utils.js
+++ utils.js
@@ -2,26 +2,26 @@ const c = (el) => document.createElement(el);
 
 const q = (el) => document.querySelector(el);
 
-const createCard = (parent, imgLink, title, price) => {
-  const wrapperEl = c("div");
-  const titleEl = c("h3");
-  const priceEl = c("p");
-  const imgEl = c("img");
+// const createCard = (parent, imgLink, title, price) => {
+//   const wrapperEl = c("div");
+//   const titleEl = c("h3");
+//   const priceEl = c("p");
+//   const imgEl = c("img");
 
-  wrapperEl.className = "wrapper";
-  titleEl.className = "title";
-  priceEl.className = "price";
-  imgEl.className = "img";
+//   wrapperEl.className = "wrapper";
+//   titleEl.className = "title";
+//   priceEl.className = "price";
+//   imgEl.className = "img";
 
-  imgEl.setAttribute("alt", title);
-  imgEl.setAttribute("src", imgLink);
+//   imgEl.setAttribute("alt", title);
+//   imgEl.setAttribute("src", imgLink);
 
-  titleEl.textContent = title;
-  priceEl.textContent = price;
+//   titleEl.textContent = title;
+//   priceEl.textContent = price;
 
-  wrapperEl.append(imgEl, titleEl, priceEl);
-  parent.appendChild(wrapperEl);
-};
+//   wrapperEl.append(imgEl, titleEl, priceEl);
+//   parent.appendChild(wrapperEl);
+// };
 
 const createFriendEl = (parent, name, photo) => {
   const wrapper = c("div");
@@ -42,22 +42,25 @@ const createMessageEl = (parent, text, sender, date, id) => {
   const textPar = c("p");
   const senderPar = c("p");
   const dataPar = c("p");
+  const deleteBtn = c("button");
 
+  deleteBtn.className = "deletemsg";
   wrapper.className = "messageCard";
   textPar.textContent = text;
   senderPar.textContent = sender;
+  deleteBtn.textContent = "x";
   // dataPar.textContent = date.split('T')[0].split('-').reverse().join('-');
   dataPar.textContent = date;
 
-  wrapper.append(textPar, senderPar, dataPar);
+  wrapper.append(deleteBtn, textPar, senderPar, dataPar);
   parent.appendChild(wrapper);
 
-  wrapper.addEventListener("click", () => {
+  deleteBtn.addEventListener("click", () => {
     DELETE("https://edgemony-backend.herokuapp.com/messages", id).then(() =>
       location.reload()
     );
   });
 };
 
-export { c, q, createCard, createFriendEl, createMessageEl };
+export { c, q, createFriendEl, createMessageEl };
 import { DELETE } from "./api.js";
